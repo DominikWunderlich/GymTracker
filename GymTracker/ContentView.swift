b7541f6a60eb59bdd51cfef660dab6155e6f0de5
Dominik Wunderlich <dw082@hdm-stuttgart.de>
2025-07-20 21:41:56 +0200

    Init Commit

--- GymTracker/ContentView.swift
+++ GymTracker/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var workouts: [String] = []
+    @State private var showingAddWorkout = false
+    @State private var newWorkoutName = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(workouts, id: \.self) { workout in Text(workout)
+            }
+            .navigationTitle("Workouts")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                showingAddWorkout = true}) {
+                    Image(systemName: "plus")
+                }
+            )
+            .sheet(isPresented: $showingAddWorkout) {
+                VStack {
+                    Text("Add New Workout")
+                        .font(.headline)
+                    TextField("Workout Name", text: $newWorkoutName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    Button("Add") {
+                        if !newWorkoutName.trimmingCharacters(in: .whitespaces).isEmpty {
+                                workouts.append(newWorkoutName)
+                                newWorkoutName = ""
+                                showingAddWorkout = false
+                        }
+                    }
+                    .padding()
+                    Button("Cancel") {
+                        newWorkoutName = ""
+                        showingAddWorkout = false
+                    }
+                }
+                .padding()
+            }
+                
+            }
         }
-        .padding()
-    }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
