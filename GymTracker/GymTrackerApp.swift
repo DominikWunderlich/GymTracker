9b4d4783562237c11ed0fe8ce2255b3149d1e58c
Dominik Wunderlich <dw082@hdm-stuttgart.de>
2025-07-20 20:06:03 +0200

    Initial Commit

---
+++ GymTracker/GymTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackerApp.swift
+//  GymTracker
+//
+//  Created by Dominik Wunderlich on 20.07.25.
+//
+
+import SwiftUI
+
+@main
+struct GymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
